dd4309ae037bce1e7a51797b445c1c307807e335
sakaitaka <hyise.71@gmail.com>
2020-02-28 12:05:17 +0900

    change path

--- restart_server.sh
+++ restart_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+source /root/.bash_profile
 ps aux | grep manage.py | grep -v grep | awk '{ print "kill -9", $2 }' | sh
 cd /root/image-converter/src
-nohup ./manage.py runserver_plus --cert-file ./cert.pem  --key-file ./privkey.pem &
+nohup /root/image-converter/src/manage.py runserver_plus --cert-file /etc/letsencrypt/live/doctor-robo.com/cert.pem  --key-file /etc/letsencrypt/live/doctor-robo.com/privkey.pem &
 
